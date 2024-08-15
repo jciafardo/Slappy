a8b1924dc6060d35836fa7e39732a82d97b88336
jciafardo <ciafardoj@gmail.com>
2024-08-14 22:23:06 -0400

    creating folders and files

--- backend/drive.ts
+++ backend/drive.ts
@@ -18,6 +18,9 @@ async function getFileContent(authClient: OAuth2Client, fileId: string): Promise
     // Return file data as a json object
     return fileData;
   } catch (error) {
+    // add a check if we have a folder named slappy
+    createFolder(authClient)
+    createFile(authClient)
     console.error('Error retrieving file content:', error);
     return JSON.parse('{"fileReadingError": "Cannot read text from file... copy some text to create a fresh slappy"}');
   }
@@ -27,14 +30,46 @@ async function writeFileContent(){
 
 }
 
-async function createFile(){
+async function createFile(authClient: OAuth2Client){
+  const drive = google.drive({ version: 'v3', auth: authClient });
+
+const fileMetadata = {
+  name: 'slappy.txt', // Name of the file with .txt extension
+  mimeType: 'text/plain', // MIME type for a text file
+};
+
+const media = {
+  mimeType: 'text/plain', // MIME type matching the content type
+  body: 'content of the file', // Content of the file
+};
+
+try {
+  const response = await drive.files.create({
+    requestBody: fileMetadata,
+    media: media, // Attach the media object for file content
+    fields: 'id', // Specify the fields to be returned in the response
+  });
   
+  const fileId = response.data.id; // Retrieve the file ID from the response
+  
+  if (typeof fileId === 'string') {
+    console.log('File Id:', fileId);
+    return fileId; // Returns the file ID as a string
+  } else {
+    // Handle unexpected response data format
+    throw new Error('Failed to retrieve the file ID');
+  }
+} catch (err) {
+  // Handle any errors that occur during the file creation process
+  console.error('Error creating file:', err);
+  throw err;
+}
 }
 
 async function createFolder(authClient: OAuth2Client): Promise<string> {
   const drive = google.drive({ version: 'v3', auth: authClient });
   const fileMetadata = {
-    name: 'Invoices',
+    name: 'Slappy',
     mimeType: 'application/vnd.google-apps.folder',
   };
   
